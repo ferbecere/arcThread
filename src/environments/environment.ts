7132a55dfa71736248ea61b7bc4aa4f325cdbbaf
ferbecere <ferran.calpe@gmail.com>
2025-08-30 23:05:02 +0200

    enviroments and services started. linking with supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: "https://fqpxwoqpcoxogdgyxdxx.supabase.co",
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxcHh3b3FwY294b2dkZ3l4ZHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1ODIyODEsImV4cCI6MjA3MjE1ODI4MX0.XmDZVp0uWuDBHVwu1tqnT5GcAj4-SeTT_HttAoyzv1Y'
+};
---
+++ src/services/supabase.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import { createClient, SupabaseClient, User, Session } from '@supabase/supabase-js';
+
+import {environment} from '../environments/environment';
+
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class SupabaseService{
+    private supabase:SupabaseClient;
+
+    constructor(){
+        this.supabase = createClient(environment.supabaseUrl,environment.supabaseAnonKey);
+    }
+
+    async signUp(email:string, password:string): Promise<User | null>{
+        const {data,error} = await this.supabase.auth.signUp({email,password});
+        if (error) throw error;
+        return data.user ?? null;
+    }
+
+    async signIn(email:string, password:string):Promise<User | null>{
+        const {data,error} = await this.supabase.auth.signInWithPassword({email,password});
+        if (error) throw error;
+        return data.user ?? null;
+    }
+
+    async signOut():Promise<void>{
+        const {error} = await this.supabase.auth.signOut();
+        if(error) throw error;
+    }
+
+    async getUser(): Promise<User | null>{
+        const {data,error} = await this.supabase.auth.getUser();
+        if(error) throw error;
+        return data.user ?? null;
+    }
+
+    onAuthStateChange(callback:(event:string, session: Session |null)=> void){
+        this.supabase.auth.onAuthStateChange((event:string,session: Session | null)=>{
+            callback(event,session);
+        })
+    }
+
+
+}
+
