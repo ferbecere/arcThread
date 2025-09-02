da9a575fcad910f9b3b841893c8e5645f77d3844
ferbecere <ferran.calpe@gmail.com>
2025-09-02 12:06:01 +0200

    position_

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
+import { LandingComponent } from './landing/landing.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:'', component:AuthComponent},
+    {path:'', component:LandingComponent},
     {path:'home', component: HomeComponent},
 
 ];
