1bf40c96d373e8d1f01d644442663c0f3e859bff
ferbecere <ferran.calpe@gmail.com>
2025-09-12 16:03:31 +0200

    components defined.

--- src/app/home/characters/character-list/character-list.component.ts
+++ src/app/home/characters/character-list/character-list.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterService } from '../../../../services/characters.service';
+import { CharacterCardComponent } from '../character-card/character-card.component';
 
 @Component({
   selector: 'app-character-list',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule,CharacterCardComponent],
   templateUrl: './character-list.component.html',
   styleUrl: './character-list.component.css'
 })
-export class CharacterListComponent {
+export class CharacterListComponent implements OnInit{
+  characters: any[] = [];
+  loading = true;
+  error: string | null = null;
+
+  constructor(private characterService: CharacterService){}
+
+  async ngOnInit(){
+      
+    try{
+      this.characters = await this.characterService.getCharacters();
+    
+    } catch(err:any){
+      this.error = err.message ?? 'Loading Characters error';
+    
+    } finally {
+      this.loading = false;
+    }
+  }
 
 }
