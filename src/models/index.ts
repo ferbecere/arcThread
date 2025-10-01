ca0c2fe73a03ca3f0e46f6359fce6bf7e407f21b
ferbecere <ferran.calpe@gmail.com>
2025-10-01 21:04:38 +0200

    revision

--- src/models/index.ts
+++ src/models/index.ts
@@ -6,5 +6,9 @@ export * from './faction.model';
 export * from './event.model';
 export * from './relationships.model';
 
+import { Character } from './character.model';
+import { Faction } from './faction.model';
+import { Event } from '@angular/router';
+
 // Tipo union para cualquier entidad del canvas
 export type CanvasCard = Character | Faction | Event;
